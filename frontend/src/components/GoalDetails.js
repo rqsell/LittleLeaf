486382bedba31338f05b996a12c3d35651663296
rqsell <68514875+rqsell@users.noreply.github.com>
2020-10-14 16:38:31 -0400

    bounce bb

--- frontend/src/components/GoalDetails.js
+++ frontend/src/components/GoalDetails.js
@@ -37,66 +37,29 @@ function GoalDetails(props) {
 
   return (
     <div>
-{/* THE MENU */}
-      <article id="theMenu">
-      <ul className="hamburgerMenu" id={open ? "clickedmenu" : ""}>
-        {/* <Menu> */}
-        <div className="x" onClick={() => setOpen(!open)}>
-          X
-        </div>
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <li>Home</li>
-        </Link>
-        <Link to="/AddAGoal" style={{ textDecoration: "none" }}>
-          <li>Add a Goal</li>
-        </Link>
-        <Link style={{ textDecoration: "none" }}>
-          <li>Log In</li>
-        </Link>
-        <li>About Us</li>
-        <li>Profile</li>
-        {/* </Menu> */}
-      </ul>
-      {/* ) : null} */}
+      {/* WELCOME MESSAGES */}
+      <article id="introbox">
+        <h4 class="goalsintro">Welcome to your Tasks page!</h4>
+        <span className="goalPageText">
+          Add a task to help you achieve your goal!
+          <br></br>
+          The task will assist you in achieving your goal {goalDetail.name} in
+          which you will {goalDetail.description} by {goalDetail.endDate}
+        </span>
+        <br></br>
+        <h4 class="goalsintro">Add a New Task to Your List!</h4>
       </article>
-{/* THE MENU/THE LOGO */}
-      <div className="addAGoalDiv">
-        <div className="goalIntro">
-          <article id="logobox2">
-            <img
-              src="./images/Copy of Little Leaf Big Tree Logo.png"
-              alt="our logo"
-              id={open ? "clickedlogo" : ""}
-              class="logo3"
-              onClick={() => setOpen(!open)}
-            />
-          </article>
 
-{/* WELCOME MESSAGES */}
-          <article id="introbox">
-            <h4 class="goalsintro">Welcome to your Tasks page!</h4>
-            <span className="goalPageText">
-              Add a task to help you achieve your goal!
-              <br></br>
-              The task will assist you in achieving your goal {
-                goalDetail.name
-              }{" "}
-              in which you will {goalDetail.description} by {goalDetail.endDate}
-            </span>
-            <br></br>
-            <h4 class="goalsintro">Add a New Task to Your List!</h4>
-          </article>
-        </div>
-        <AddATask {...props} />
+      <AddATask {...props} />
 
-        <SeeTask {...props} />
+      <SeeTask {...props} />
 
-        {/* <div className="Taskss">{showMyTasks()}</div> */}
-        {/**Update end dates onChange of calandar */}
+      {/* <div className="Taskss">{showMyTasks()}</div> */}
+      {/**Update end dates onChange of calandar */}
 
-        {/* <MyCalendar {...props} /> */}
-        {/* <Fullcalendar {...props} /> */}
-        </div>
+      {/* <MyCalendar {...props} /> */}
+      {/* <Fullcalendar {...props} /> */}
     </div>
-  )}
+  );
+}
 export default GoalDetails;
